b18aaf28c3e6bf42ce0a0167632364da9327f6db
s soundarya <ssoundarya2010@gmail.com>
2019-04-10 09:20:23 +0000

    declaration of fragment array

--- controller/View1.controller.js
+++ controller/View1.controller.js
@@ -1,17 +1,20 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast"
-], function(Controller, MessageToast) {
+], function (Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("zcustomControl_1.controller.View1", {
-		onRatingChanged: function(oEvent) {
+		onRatingChanged: function (oEvent) {
 			var value = oEvent.getParameter("value");
-			MessageToast.show("Your new Rating : " + value); 
+			MessageToast.show("Your new Rating : " + value);
 		},
-		onInit : function(){
-			
-			var a=10;
+		onInit: function () {
+
+			var a = 10;
+
+			this._fragments = [];
+
 		}
 	});
 });
